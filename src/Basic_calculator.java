7bdcc16072d16b634c9898fd4407cca35711ff91
matchat1 <matchat1@udayton.edu>
2023-05-15 18:56:33 -0400

    Message is written

--- src/Basic_calculator.java
+++ src/Basic_calculator.java
@@ -32,6 +32,7 @@ public class Basic_calculator {
             }
             System.out.println("Percentage");
             System.out.println("Tester");
+            System.out.println("Commands from here itself");
         }
 
 
