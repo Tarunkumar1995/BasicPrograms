b4f6d9bf91db53fc278a4a864c1c6971cfa04a8a
matchat1 <matchat1@udayton.edu>
2023-05-15 18:30:27 -0400

    Initial Commit

---
+++ src/Basic_calculator.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Basic_calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        int b = sc.nextInt();
+        System.out.println("Enter the Operation");
+        String operation = sc.next();
+        switch (operation){
+            case "add":
+                System.out.println("Addition of two numbers " + (a +b));
+                break;
+            case "Sub":
+                System.out.println("Subtraction of two numbers" + (a - b));
+                break;
+            case "multiply":
+                System.out.println("Multiplication of two numbers" + (a * b));
+                break;
+
+        }
+
+
+    }
+}
+
